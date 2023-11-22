6dc4196cff9ddbf4521cc2abd94677aa0518edf1
abigailkeller <abigail.g.keller1@gmail.com>
2023-11-22 23:50:02 +0000

    added gamma dist. for traditional data

---
+++ inst/stan/traditional_catchability_gamma.stan
@@ -0,0 +1,63 @@
+data{/////////////////////////////////////////////////////////////////////
+    int<lower=1> C;    // number of trap samples
+    array[C] int<lower=1> R;   // index of locations for traditional samples
+    int<lower=1> Nloc;   // total number of locations
+    array[C] int<lower=0> E;   // number of animals in sample C
+    int<lower=0> nparams;  // number of gear types
+    matrix[C,nparams] mat;  // matrix of gear type integers
+
+}
+
+parameters{/////////////////////////////////////////////////////////////////////
+    array[Nloc] real<lower=0> alpha;  // alpha param for gamma distribution
+    array[Nloc] real <lower=0> beta;  // beta param for gamma distribution
+    vector<lower=-0.99999>[nparams] q_trans; // catchability coefficients
+    }
+
+transformed parameters{/////////////////////////////////////////////////////////////////////
+    vector<lower=0>[C] coef;
+    array[C] real<lower=0> E_trans;
+
+    for(i in 1:C){
+      coef[i] = 1 + dot_product(mat[i],q_trans);
+    }
+
+    for(j in 1:C){
+      E_trans[j] = E[j] + 0.0000000000001;
+    }
+}
+
+model{/////////////////////////////////////////////////////////////////////
+
+
+    for(j in 1:C){
+
+      E_trans[j] ~ gamma(coef[j]*alpha[R[j]],beta[R[j]]); // Eq. 1.1
+    }
+
+    beta ~ gamma(0.25,0.25);
+    alpha ~ gamma(0.25,0.25);
+
+}
+
+generated quantities{
+  vector[nparams] q;
+  vector[C] log_lik;
+  matrix[Nloc,nparams+1] mu;  // matrix of catch rates
+
+  q = q_trans + 1;
+
+  for(j in 1:Nloc){
+    mu[j,1] = alpha[j]/beta[j];
+  }
+
+  for(i in 1:nparams){
+    mu[,i+1] = to_vector(mu[,1])*q[i];
+  }
+
+    for(j in 1:C){
+          log_lik[j] = gamma_lpdf(E_trans[j] | coef[j]*alpha[R[j]], beta[R[j]]); //store log likelihood of traditional data given model
+      }
+
+}
+
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,30 +12,42 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 
+RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_catchability_gamma_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_catchability_negbin_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_catchability_pois_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_cov_catchability_gamma_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_cov_catchability_negbin_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_cov_catchability_pois_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_cov_gamma_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_cov_negbin_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_cov_pois_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_gamma_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_negbin_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4joint_binary_pois_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4traditional_catchability_gamma_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4traditional_catchability_negbin_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4traditional_catchability_pois_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4traditional_gamma_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4traditional_negbin_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4traditional_pois_mod();
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_rcpp_module_boot_stan_fit4joint_binary_catchability_gamma_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_catchability_gamma_mod, 0},
     {"_rcpp_module_boot_stan_fit4joint_binary_catchability_negbin_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_catchability_negbin_mod, 0},
     {"_rcpp_module_boot_stan_fit4joint_binary_catchability_pois_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_catchability_pois_mod, 0},
+    {"_rcpp_module_boot_stan_fit4joint_binary_cov_catchability_gamma_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_cov_catchability_gamma_mod, 0},
     {"_rcpp_module_boot_stan_fit4joint_binary_cov_catchability_negbin_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_cov_catchability_negbin_mod, 0},
     {"_rcpp_module_boot_stan_fit4joint_binary_cov_catchability_pois_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_cov_catchability_pois_mod, 0},
+    {"_rcpp_module_boot_stan_fit4joint_binary_cov_gamma_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_cov_gamma_mod, 0},
     {"_rcpp_module_boot_stan_fit4joint_binary_cov_negbin_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_cov_negbin_mod, 0},
     {"_rcpp_module_boot_stan_fit4joint_binary_cov_pois_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_cov_pois_mod, 0},
+    {"_rcpp_module_boot_stan_fit4joint_binary_gamma_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_gamma_mod, 0},
     {"_rcpp_module_boot_stan_fit4joint_binary_negbin_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_negbin_mod, 0},
     {"_rcpp_module_boot_stan_fit4joint_binary_pois_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4joint_binary_pois_mod, 0},
+    {"_rcpp_module_boot_stan_fit4traditional_catchability_gamma_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4traditional_catchability_gamma_mod, 0},
     {"_rcpp_module_boot_stan_fit4traditional_catchability_negbin_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4traditional_catchability_negbin_mod, 0},
     {"_rcpp_module_boot_stan_fit4traditional_catchability_pois_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4traditional_catchability_pois_mod, 0},
+    {"_rcpp_module_boot_stan_fit4traditional_gamma_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4traditional_gamma_mod, 0},
     {"_rcpp_module_boot_stan_fit4traditional_negbin_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4traditional_negbin_mod, 0},
     {"_rcpp_module_boot_stan_fit4traditional_pois_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4traditional_pois_mod, 0},
     {NULL, NULL, 0}
